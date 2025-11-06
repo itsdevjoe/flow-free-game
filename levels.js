6afe396b0ea712b2eb686b980bb8c7313b255a02
itsdevjoe <jobayerpacific@gmail.com>
2025-11-05 18:04:14 -0800

    Create levels.js

---
+++ levels.js
@@ -0,0 +1,24 @@
+// Level definitions for Flow Free game
+const levels = [
+  {
+    size: 5,
+    connections: [
+      { color: 'red', points: [[0, 0], [4, 4]] },
+      { color: 'blue', points: [[0, 4], [4, 0]] }
+    ]
+  },
+  {
+    size: 6,
+    connections: [
+      { color: 'green', points: [[0, 1], [5, 5]] },
+      { color: 'orange', points: [[2, 2], [3, 4]] }
+    ]
+  },
+  {
+    size: 5,
+    connections: [
+      { color: 'purple', points: [[0, 2], [4, 2]] },
+      { color: 'yellow', points: [[1, 1], [3, 3]] }
+    ]
+  }
+];
